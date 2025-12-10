c3e772f9de7a0900f10935dc9723204287cbdb96
chandru-tirumalaraju <95275111+chandru-tirumalaraju@users.noreply.github.com>
2025-12-10 19:53:24 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 echo "In shell script"
-aws cloudformation deploy --template-file mounu.yml --stack-name "mounika"
+aws cloudformation deploy --template-file mounu.yml --stack-name 'mounika'
