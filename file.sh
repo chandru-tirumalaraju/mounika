500144941c1e08f0577fe10ca64566b269862df8
chandru-tirumalaraju <chandrasekhar.tirumalaraju@gmail.com>
2025-12-12 08:25:58 +0530

    creating ec2 instance

--- file.sh
+++ file.sh
@@ -1,8 +1,7 @@
 #!bin/bash
-echo "In shell script"
 stackname='mounu-stack'
-aws cloudformation deploy --template-file mounu.yml --stack-name ${stackname}
-
-
-
-
+stackname2='seconone'
+echo "creating s3bucket"
+aws cloudformation deploy --template-file mounu.yml --stack-name ${stackname} --parameter-override BucketName='chandumounubuc'
+echo "creating ec2instance"
+aws cloudformation deploy --template-file EC2.yml --stack-name ${stackname2} --parameter-override amiid='ami-00ca570c1b6d79f36' InstanceType='t3.micro'
